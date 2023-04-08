5d45343679c55aed61ae9e8e990e2c4dbece1d2a
Jake Wood <jzwood14@yahoo.com>
2023-04-07 23:06:40 -0400

    wip config.dev

--- config/config.exs
+++ config/config.exs
@@ -9,7 +9,7 @@ import Config
 
 # Configures the endpoint
 config :live_buggies, LiveBuggiesWeb.Endpoint,
-  url: [host: "0.0.0.0"],
+  url: [host: "localhost", host: "https://buggy-buggies.gigalixirapp.com"],
   render_errors: [view: LiveBuggiesWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: LiveBuggies.PubSub,
   live_view: [signing_salt: "ybLgsvPZ"]
