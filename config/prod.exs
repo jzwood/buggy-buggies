b5a10f0df49ce610e7651e7084d37e681cf298e8
Jake Wood <jzwood14@yahoo.com>
2023-04-07 23:28:55 -0400

    wip

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :live_buggies, LiveBuggiesWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  url: [host: "https://buggy-buggies.gigalixirapp.com", port: 443],
-  https: [port: 443]
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
